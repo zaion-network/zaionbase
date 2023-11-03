fd4877ba997f0c0404cf4bc52fb0d9f0ec1fee21
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-11-03 19:47:57 +0100

    crea test bun

--- src/using DeeperUtilities/using DeepUtilites/using LowUtilities/using Utilities/using HighUtilities/JavaScript/MapUtils.test.ts
+++ src/using DeeperUtilities/using DeepUtilites/using LowUtilities/using Utilities/using HighUtilities/JavaScript/MapUtils.test.ts
@@ -1,10 +1,25 @@
-import { MapUtils } from "./MapUtils";
+import { describe, it, expect } from "bun:test";
+import { MapUtils, ExtendedMap } from "./MapUtils";
 
-const stringifyMap = () => {
-  const map = new Map();
-  map.set(1, "ciao");
-  map.set(2, "miao");
-  const string = MapUtils.stringifyMap(map);
-  console.log(string);
-};
-stringifyMap();
+describe("stringifyMap default behave", () => {
+  it("dovrebbe tornare il valore aspettato", () => {
+    const map = new Map();
+    map.set(1, "ciao");
+    map.set(2, "miao");
+    const string = MapUtils.stringifyMap(map);
+    const EXPECTED = '[[1,"ciao"],[2,"miao"]]';
+    expect(string).toEqual(EXPECTED);
+  });
+});
+
+describe("stringifyMap object behave", () => {
+  it("dovrebbe tornare il valore aspettato", () => {
+    const map = new Map();
+    map.set(1, "ciao");
+    map.set(2, "miao");
+    const string = MapUtils.stringifyMap(map, "object");
+    const EXPECTED = `{"1":"ciao","2":"miao"}`;
+    expect(string).toEqual(EXPECTED);
+  });
+});
+
