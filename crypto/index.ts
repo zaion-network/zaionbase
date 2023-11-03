1ec7fb884b2ddd5bc834cf505e7d087e6738de92
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-11-03 10:20:15 +0100

    crea sentrypoint

---
+++ crypto/index.ts
@@ -0,0 +1,3 @@
+import { hmacIt } from "../src/using DeeperUtilities/using DeepUtilites/using LowUtilities/using Utilities/using HighUtilities/Node/Crypto/hmacit";
+import { hashIt } from "../src/using DeeperUtilities/using DeepUtilites/using LowUtilities/using Utilities/using HighUtilities/Node/Crypto/hashit";
+export { hashIt, hmacIt };
