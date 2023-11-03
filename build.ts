a01564f209042ed09010c349a31effdd112cbe66
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-11-03 10:20:26 +0100

    aggiunge build

--- build.ts
+++ build.ts
@@ -5,4 +5,10 @@ const res = await Bun.build({
   outdir: "dist",
   target: "bun",
 });
+const cryptoNode = await Bun.build({
+  entrypoints: ["./crypto/index.ts"],
+  outdir: "dist/crypto",
+  target: "node",
+});
 console.log(res);
+console.log(cryptoNode);
