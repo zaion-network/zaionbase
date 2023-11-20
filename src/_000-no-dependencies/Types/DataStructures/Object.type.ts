bd3920ccfc30458958d61673d04c971942e95777
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-11-20 18:09:34 +0100

    fromObjectToArray

--- src/_000-no-dependencies/Types/DataStructures/Object.type.ts
+++ src/_000-no-dependencies/Types/DataStructures/Object.type.ts
@@ -1,4 +1,6 @@
+import { primitives } from "../Primitive.type";
+
 export namespace Object {
-  export type KeyValueObj = { [k: PropertyKey]: any };
+  export type KeyValueObj = { [k: PropertyKey]: primitives | KeyValueObj };
   export type fromObjetToTupleUnion<O> = { [K in keyof O]: [K, O[K]] }[keyof O];
 }
--- src/_000-no-dependencies/utils/fromObjToArray.ts
+++ src/_000-no-dependencies/utils/fromObjToArray.ts
@@ -1,5 +1,13 @@
+import { Array as A, Object as O } from "../Types/DataStructures/Mixer.type";
+import { Object as Ob } from "../Types/DataStructures/Object.type";
+
 export function fromObjToArray<
-  T extends { [k: string]: string | { [k: string]: any } },
+  T extends Ob.KeyValueObj,
+  K extends keyof T,
+  V extends T[K] | [K, T[K]]
+>(obj: T): T extends A.toObj<infer A> ? A : [K, V][];
+export function fromObjToArray<
+  T extends Ob.KeyValueObj,
   K extends keyof T,
   V extends T[K] | [K, T[K]]
 >(obj: T): [K, V][] {
@@ -7,7 +15,7 @@ export function fromObjToArray<
   const keys: K[] = Object.keys(obj) as K[];
   const array: [K, V][] = [];
   keys.forEach(k => {
-    if (typeof obj[k] === "object" && Object.keys(obj[k]).length > 0) {
+    if (typeof obj[k] === "object" && Object.keys(obj[k]!).length > 0) {
       const nestedArray = fromObjToArray(obj[k] as { [k: string]: any });
       nestedArray.forEach(([nestedK, nestedV]) => {
         array.push([k, [nestedK, nestedV] as V]);
