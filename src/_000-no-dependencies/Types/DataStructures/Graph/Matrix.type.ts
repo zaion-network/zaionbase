b2223125e8ff63940b6caf48b9566064837a0197
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-11-20 20:39:01 +0100

    aggiunge Matrix.type.ts

---
+++ src/_000-no-dependencies/Types/DataStructures/Graph/Matrix.type.ts
@@ -0,0 +1,16 @@
+type Node = number;
+type AdjacencyMatrix = Node[][];
+const adjacencyMatrix: AdjacencyMatrix = [
+  [0, 1, 1, 0, 0],
+  [0, 0, 0, 1, 0],
+  [0, 0, 0, 0, 1],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+];
+const adjacencyMatrix2: AdjacencyMatrix = [
+  [0, 1, 1, 0, 0],
+  [0, 0, 0, 1, 0],
+  [0, 0, 0, 0, 1],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+];
