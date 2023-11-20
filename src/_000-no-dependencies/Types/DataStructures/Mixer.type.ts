9e63de27816508c92600d78de8e6bb73266e31d0
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-11-20 11:22:16 +0100

    enhance

--- src/_000-no-dependencies/Types/DataStructures/Mixer.type.ts
+++ src/_000-no-dependencies/Types/DataStructures/Mixer.type.ts
@@ -2,13 +2,23 @@ import { UnionStuff } from "../UnionStuff.type";
 import { Array as A } from "./Array.type";
 import { Object as O } from "./Object.type";
 import { Pair } from "./Tuple.type";
+// import { Pair } from "./Tuple.type";
 
 // // -------------------------------------- Object
 // export type KeyValueObj = { [k: PropertyKey]: any };
 
+type primitives = string | number | boolean | undefined | null;
+
 export namespace Object {
+  type checkAndRecurse<T, K extends keyof T> = T[K] extends Pair.KeyValue
+    ? toMap<Array.toObj<[T[K]]>>
+    : T[K];
   // OBJ => MAP
   export type toMap<T> = {
+    set<K extends keyof T>(key: K, value: T[K]): void;
+    get<K extends keyof T>(key: K): checkAndRecurse<T, K>;
+  };
+  export type toMap2<T> = {
     set<K extends keyof T>(key: K, value: T[K]): void;
     get<K extends keyof T>(key: K): T[K];
   };
@@ -31,12 +41,18 @@ export namespace Object {
 // -------------------------------------- Array
 export namespace Array {
   // TUPLEARR => OBJ
+  type checkAndRecurse<T extends A.KeyValueArr> = T extends Array<any>
+    ? toObj<T>
+    : T;
   export type toObj<T extends A.KeyValueArr> = {
-    [K in A.keysInKeyValueArr<T>]: A.extractor<T, K>;
+    [K in A.keysInKeyValueArr<T>]: checkAndRecurse<A.extractor<T, K>>;
   };
 
   // TUPLEARR => MAP
-  export type toMap<T extends A.KeyValueArr> = Object.toMap<toObj<T>>;
+  type toMapSub<T extends A.KeyValueArr> = {
+    [K in A.keysInKeyValueArr<T>]: A.extractor<T, K>;
+  };
+  export type toMap<T extends A.KeyValueArr> = Object.toMap<toMapSub<T>>;
 }
 
 // -------------------------------------- Map
