03c35636db79a36f6e5403a02bcbc5165567eb89
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-11-21 18:51:08 +0100

    aggiunge

--- build.ts
+++ build.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env bun
 
-const res = await Bun.build({
+const index = await Bun.build({
   entrypoints: ["./src/index.ts"],
   outdir: "dist",
   target: "bun",
@@ -8,19 +8,24 @@ const res = await Bun.build({
 });
 
 const arrayMap = await Bun.build({
-  entrypoints: ["./array/index.ts", "./map/index.ts", "./tuple/index.ts"],
+  entrypoints: [
+    "./array/index.ts",
+    "./map/index.ts",
+    "./tuple/index.ts",
+    "./mixins/index.ts",
+  ],
   outdir: "dist",
   target: "bun",
   format: "esm",
 });
 
-const cryptoNode = await Bun.build({
+const crypto = await Bun.build({
   entrypoints: ["./crypto/index.ts"],
   outdir: "dist/crypto",
   target: "node",
 });
 
-const serverNode = await Bun.build({
+const server = await Bun.build({
   entrypoints: ["./server/node/index.ts"],
   outdir: "dist/server",
   target: "node",
@@ -32,8 +37,8 @@ const web = await Bun.build({
   target: "browser",
 });
 
-console.log(res.success);
-console.log(arrayMap.success);
-console.log(cryptoNode.success);
-console.log(serverNode.success);
-console.log(web.success);
+console.log("res", index.success);
+console.log("array/map/tuple/mixins", arrayMap.success);
+console.log("crypto", crypto.success);
+console.log("server", server.success);
+console.log("web", web.success);
---
+++ mixins/index.ts
@@ -0,0 +1,2 @@
+import { Mixins } from "../src/_000-no-dependencies/Mixins";
+export default Mixins.Mix;
