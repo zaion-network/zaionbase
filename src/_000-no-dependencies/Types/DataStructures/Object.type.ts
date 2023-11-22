12d3d0a04fe991e1c2901e0796ab62b4af922845
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-11-22 12:33:12 +0100

    import da utils

--- src/_000-no-dependencies/Types/DataStructures/Object.type.ts
+++ src/_000-no-dependencies/Types/DataStructures/Object.type.ts
@@ -1,9 +1,10 @@
 import { primitives } from "../Primitive.type";
 import { Object as O } from "./Mixer.type";
+import { fromObjetToTupleUnion as fottu } from "./utils/fromObjectToTupleUnion";
 
 export namespace Object {
   export type KeyValueObj = { [k: PropertyKey]: primitives | KeyValueObj };
-  export type fromObjetToTupleUnion<O> = { [K in keyof O]: [K, O[K]] }[keyof O];
+  export type fromObjetToTupleUnion<O> = fottu<O>;
   export type toArr<T> = O.toArr<T>;
   export type toMap<T> = O.toMap<T>;
   export type toMap2<T> = O.toMap2<T>;
